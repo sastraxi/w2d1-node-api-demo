e1fdaf7fd526447b90e0404b3c87ce07385d1d11
Cameron Gorrie <sastraxi@gmail.com>
2018-10-30 02:07:44 -0400

    Add some more notes

--- src/show-table.js
+++ src/show-table.js
@@ -9,13 +9,13 @@ const HEADERS = [
 ];
 
 module.exports = function showTable(conditions) {
-  const table = new Table({
-    head: HEADERS,
-  });
+  const table = new Table({ head: HEADERS });
+ 
+  // table acts as an array; add arrays of 4 elements (one for each header)
   Object.keys(conditions).forEach(city => table.push([
     city,
     `${conditions[city].temperature} °C`,
-    conditions[city].precipIntensity || '--',
+    conditions[city].precipIntensity || '--', // using falsey-ness of 0
     `${conditions[city].cloudCover * 100}%`,
   ]));
   console.log(table.toString());
