b94021a5c037eb51e1836403958b1c8099019c49
Cameron Gorrie <sastraxi@gmail.com>
2018-10-30 02:11:18 -0400

    make things a little more readable

--- src/show-stats.js
+++ src/show-stats.js
@@ -1,19 +1,27 @@
 module.exports = function showStats(conditions) {
   let rainiest = null, cloudiest = null, warmest = null, coldest = null;
+  
+  // Object.keys(object) returns an array with each key in it (as strings)
   Object.keys(conditions).forEach(function(city) {
-    if (!rainiest || conditions[city].precipIntensity > conditions[rainiest].precipIntensity) {
+    
+    // use es6 destructuring to cut down on line length
+    const { precipIntensity, cloudCover, temperature } = conditions[city];
+    
+    if (!rainiest || precipIntensity > conditions[rainiest].precipIntensity) {
       rainiest = city;
     }
-    if (!cloudiest || conditions[city].cloudCover > conditions[cloudiest].cloudCover) {
+    if (!cloudiest || cloudCover > conditions[cloudiest].cloudCover) {
       cloudiest = city;
     }
-    if (!warmest || conditions[city].temperature > conditions[warmest].temperature) {
+    if (!warmest || temperature > conditions[warmest].temperature) {
       warmest = city;
     }
-    if (!coldest || conditions[city].temperature < conditions[coldest].temperature) {
+    if (!coldest || temperature < conditions[coldest].temperature) {
       coldest = city;
     }
+    
   });
+
   console.log(`The warmest city is ${warmest}`);
   console.log(`The coldest city is ${coldest}`);
   console.log(`The cloudiest city is ${cloudiest}`);
